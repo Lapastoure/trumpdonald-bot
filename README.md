7539d53c39ba52d32ba2fb063dac30d55eee0bbb
Drew Engelson <drew@engelson.net>
2016-03-04 09:47:21 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Automate TrumpDonald.org
+
+Simple selenium script that forever blows Donald's strange hair.
+
+## Set-up
+
+Install dependencies in a Python virtualenv:
+
+    `pip install -r requirements.txt`
+
+Run it:
+
+    `python trump.py`
---
+++ trump.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+import time
+
+wd = WebDriver()
+wd.set_window_size(1280, 1000)
+wd.implicitly_wait(15)
+
+def wait(s):
+    time.sleep(s)
+
+try:
+    wd.get("http://trumpdonald.org/")
+
+    while True:
+        wd.find_element_by_id("can").click()
+        wait(0.1)
+finally:
+    raise Exception("Test exited.")
+
+wd.quit()
